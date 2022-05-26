167d1b55f16909c72fd20a6e0759810c79ec7c4d
kehade <kehade91@yahoo.com>
2022-05-26 13:40:46 +0100

    first commit

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is an app to find the latest DJ and other msical events</p>
+      <p>Version: 1.0.0</p>
+    </div>
+  )
+}
+
+export default About
